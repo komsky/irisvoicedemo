bbcd5f6075e1ac32053e58a5b96b7f8793e6f9cc
Balazs Cseh <balazs.cseh89@gmail.com>
2017-05-08 00:00:25 +0100

    quick test

--- index.js
+++ index.js
@@ -1,26 +1,48 @@
 require('babel-register');
 require('dotenv').load();
 
-// const app = require('./src/server')
+const app = require('./src/server')
+
+const port = process.env.PORT || 3000
+
+
+app.post('/', (req, res) => {
+  let data = '';
+  req.on('data', (chunk) => {
+    data += chunk
+  })
+  req.on('end', () => {
+    console.log('POST >>>>', data)
+  })
+})
+app.put('/', (req, res) => {
+  let data = '';
+  req.on('data', (chunk) => {
+    data += chunk
+  })
+  req.on('end', () => {
+    console.log('PUT >>>>', data)
+  })
+})
+
+app.listen(port, () => {
+  console.log(`Server is listening on port ${port}`)
+})
+
+// const Alexa = require('alexa-sdk');
 //
-// const port = process.env.PORT || 3000
-// app.listen(port, () => {
-//   console.log(`Server is listening on port ${port}`) // })
-
-const Alexa = require('alexa-sdk');
-
-module.exports.handler = (event, context) => {
-  const alexa = Alexa.handler(event, context);
-  alexa.APP_ID = process.env.APP_ID;
-  alexa.registerHandlers(handlers);
-  alexa.execute();
-};
-
-const handlers = {
-  'LaunchRequest': () => {
-    this.emit('GetHotelInformation');
-  },
-  'GetHotelInformation': () => {
-    this.emit(':tell', 'We are at Hendon Hotel!')
-  }
-}
+// exports.handler = (event, context, callback) => {
+//   const alexa = Alexa.handler(event, context);
+//   alexa.APP_ID = process.env.APP_ID;
+//   alexa.registerHandlers(handlers);
+//   alexa.execute();
+// };
+//
+// const handlers = {
+//   'LaunchRequest': function() {
+//     this.emit('GetHotelInformation');
+//   },
+//   'GetHotelInformation': function() {
+//     this.emit(':tell', 'We are at Hendon Hotel!')
+//   }
+// }
