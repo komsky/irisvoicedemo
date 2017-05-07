ae3ddb03aab1a7c9f13cdc8559524b9999f46792
Balazs Cseh <balazs.cseh89@gmail.com>
2017-05-08 00:33:56 +0100

    killing off alexa-sdk

--- index.js
+++ index.js
@@ -17,21 +17,3 @@ app.post('/', (req, res) => {
 app.listen(port, () => {
   console.log(`Server is listening on port ${port}`)
 })
-
-// const Alexa = require('alexa-sdk');
-//
-// exports.handler = (event, context, callback) => {
-//   const alexa = Alexa.handler(event, context);
-//   alexa.APP_ID = process.env.APP_ID;
-//   alexa.registerHandlers(handlers);
-//   alexa.execute();
-// };
-//
-// const handlers = {
-//   'LaunchRequest': function() {
-//     this.emit('GetHotelInformation');
-//   },
-//   'GetHotelInformation': function() {
-//     this.emit(':tell', 'We are at Hendon Hotel!')
-//   }
-// }
