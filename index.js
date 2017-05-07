6fdf315bf5e70d3307a8f9f0a0cab36ae204cfea
Balazs Cseh <balazs.cseh89@gmail.com>
2017-05-07 23:34:57 +0100

    disabling express for now, trying alexa SDK

--- index.js
+++ index.js
@@ -1,8 +1,26 @@
-require('babel-register')
+require('babel-register');
+require('dotenv').load();
 
-const app = require('./src/server')
+// const app = require('./src/server')
+//
+// const port = process.env.PORT || 3000
+// app.listen(port, () => {
+//   console.log(`Server is listening on port ${port}`) // })
 
-const port = process.env.PORT || 3000
-app.listen(port, () => {
-  console.log(`Server is listening on port ${port}`)
-})
+const Alexa = require('alexa-sdk');
+
+module.exports.handler = (event, context) => {
+  const alexa = Alexa.handler(event, context);
+  alexa.APP_ID = process.env.APP_ID;
+  alexa.registerHandlers(handlers);
+  alexa.execute();
+};
+
+const handlers = {
+  'LaunchRequest': () => {
+    this.emit('GetHotelInformation');
+  },
+  'GetHotelInformation': () => {
+    this.emit(':tell', 'We are at Hendon Hotel!')
+  }
+}
