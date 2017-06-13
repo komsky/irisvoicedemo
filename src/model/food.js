9f7f0bb8d674ee3509ab61c478635d73b0cfa5ea
Balazs Cseh <balazs.cseh89@gmail.com>
2017-06-13 18:50:32 +0100

    amending copy

--- src/model/food.js
+++ src/model/food.js
@@ -3,26 +3,26 @@ export default {
   slots: {
     mainsOptions: {
       options: [ 'Flat Iron Steak', 'Falafel', '12 oz Sirloin Steak' ],
-      invalidReprompt: 'That option is unavailable. Please choose another.',
-      reprompt: 'I didn\'t quite catch that. Could you repeat please?'
+      invalidReprompt: 'That option is unavailable. Please choose another. ',
+      reprompt: 'I didn\'t quite catch that. Could you repeat please? '
     },
     cookingOptions: {
       options: [ 'Rare', 'Medium Rare', 'Medium', 'Well Done' ],
-      prompt: 'How would you like it coooked?',
-      invalidReprompt: 'That option is unavailable. Please choose another.',
-      reprompt: 'I didn\'t quite catch that. Could you repeat please?'
+      prompt: 'How would you like it cooked? ',
+      invalidReprompt: 'That option is unavailable. Please choose another. ',
+      reprompt: 'I didn\'t quite catch that. Could you repeat please? '
     },
     sauceOptions: {
       options: [ 'No Sauce', 'Horseradish', 'Bearnaise', 'Peppercorn' ],
-      prompt: 'What sauce would you like with it?',
-      invalidReprompt: 'That option is unavailable. Please choose another.',
-      reprompt: 'I didn\'t quite catch that. Could you repeat please?'
+      prompt: 'What sauce would you like with it? ',
+      invalidReprompt: 'That option is unavailable. Please choose another. ',
+      reprompt: 'I didn\'t quite catch that. Could you repeat please? '
     },
     sideOptions: {
       options: [ 'Roasted Aubergine', 'Creamed Spinach', 'Salad', 'Fries' ],
-      prompt: 'What side would you like with it?',
-      invalidReprompt: 'That option is unavailable. Please choose another.',
-      reprompt: 'I didn\'t quite catch that. Could you repeat please?'
+      prompt: 'What side would you like with it? ',
+      invalidReprompt: 'That option is unavailable. Please choose another. ',
+      reprompt: 'I didn\'t quite catch that. Could you repeat please? '
     },
   },
   required: [
