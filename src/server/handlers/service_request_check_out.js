9cf27dbb8cbe93217cacabd464ddd7ba79093fc9
Golan Shay <golanshy@gmail.com>
2017-06-21 15:49:23 +0100

    changes

--- src/server/handlers/service_request_check_out.js
+++ src/server/handlers/service_request_check_out.js
@@ -9,51 +9,52 @@ const service_request = path([ 'HotelItems', 'service_requests', 'check_out' ], 
 
 const doRequestCheckOut = async (payload) => {
 
-const { intent: { slots: { confirmationSlot: {value} }, confirmationStatus }, dialogState } = payload.request
-  if (value !== null) {
+    const { intent: { slots: { confirmationSlot: {value} }, confirmationStatus }, dialogState } = payload.request
+    
+    if (value !== null) {
 
-    if (value === 'yes' || value === 'confirmed' || value === 'confirm') {
-        payload = {
-          categoryItemCode: service_request.code,
-          deliveryDateTime: new Date().toISOString(),
-          isDeliveryDateTimeUtc: true
-        }
-        console.log('Payload data: ' , payload)
+      if (value === 'yes' || value === 'confirmed' || value === 'confirm') {
+          payload = {
+            categoryItemCode: service_request.code,
+            deliveryDateTime: new Date().toISOString(),
+            isDeliveryDateTimeUtc: true
+          }
+          console.log('Payload data: ' , payload)
 
-        //const res = await put(addServiceRequest.path, payload)
-        const res = put(addServiceRequest.path, payload)
-        //const orderId = res.responses[0][addServiceRequest.key].content.order.id
-        //const returnText = `Your service request for ${service_request.name} was succesfully submitted; your request number is ${orderId}`
-        const returnText = `Your service request for ${service_request.name} was succesfully submitted`
+          //const res = await put(addServiceRequest.path, payload)
+          const res = put(addServiceRequest.path, payload)
+          //const orderId = res.responses[0][addServiceRequest.key].content.order.id
+          //const returnText = `Your service request for ${service_request.name} was succesfully submitted; your request number is ${orderId}`
+          const returnText = `Your service request for ${service_request.name} was succesfully submitted`
+          
+          console.log('returnText: ' , returnText)
+          //console.log('res.sessionToken: ' , res.sessionToken)
+          return {
+            text: returnText,
+            options: { shouldEndSession: true },
+            session: res.session
+          }
+      } else if (value === 'no' || value === 'not' || value === 'nope') {
         
-        console.log('returnText: ' , returnText)
-        //console.log('res.sessionToken: ' , res.sessionToken)
         return {
-          text: returnText,
-          options: { shouldEndSession: true },
-          session: res.session
+          text: item.rejectionMsg,
+          options: { shouldEndSession: true }
         }
-    } else if (value === 'no' || value === 'not' || value === 'nope') {
-      
-      return {
-        text: item.rejectionMsg,
-        options: { shouldEndSession: true }
       }
     }
-  }
-  
-  return {
-      directives: [
-        {
-          type: 'Dialog.ElicitSlot',
-          slotToElicit: 'confirmationSlot'
-        }
-      ],
-      text: item.confirmationMsg,
-      reprompt: item.repromptMsg,
-      options: { shouldEndSession: false },
-      session: {}  
-  }
+    
+    return {
+        directives: [
+          {
+            type: 'Dialog.ElicitSlot',
+            slotToElicit: 'confirmationSlot'
+          }
+        ],
+        text: item.confirmationMsg,
+        reprompt: item.repromptMsg,
+        options: { shouldEndSession: false },
+        session: {}  
+    }
 }
 
 export default doRequestCheckOut
--- src/server/handlers/service_request_ironing.js
+++ src/server/handlers/service_request_ironing.js
@@ -9,26 +9,52 @@ const service_request = path([ 'HotelItems', 'service_requests', 'ironing' ], se
 
 const doRequestIroning = async (payload) => {
 
-    payload = {
-        categoryItemCode: service_request.code,
-        deliveryDateTime: new Date().toISOString(),
-        isDeliveryDateTimeUtc: true,
-    }
-    console.log('Payload data: ' , payload)
-
-    //const res = await put(addServiceRequest.path, payload)
-    const res = put(addServiceRequest.path, payload)
-    //const orderId = res.responses[0][addServiceRequest.key].content.order.id
-    //const returnText = `Your service request for ${service_request.name} was succesfully submitted; your request number is ${orderId}`
-    const returnText = `Your request for ${service_request.name} was succesfully submitted`
+    const { intent: { slots: { confirmationSlot: {value} }, confirmationStatus }, dialogState } = payload.request
     
-    console.log('returnText: ' , returnText)
-    //console.log('res.sessionToken: ' , res.sessionToken)
-  return {
-    text: returnText,
-    options: { shouldEndSession: true },
-    session: res.session
-  }
+    if (value !== null) {
+
+      if (value === 'yes' || value === 'confirmed' || value === 'confirm') {
+          payload = {
+            categoryItemCode: service_request.code,
+            deliveryDateTime: new Date().toISOString(),
+            isDeliveryDateTimeUtc: true
+          }
+          console.log('Payload data: ' , payload)
+
+          //const res = await put(addServiceRequest.path, payload)
+          const res = put(addServiceRequest.path, payload)
+          //const orderId = res.responses[0][addServiceRequest.key].content.order.id
+          //const returnText = `Your service request for ${service_request.name} was succesfully submitted; your request number is ${orderId}`
+          const returnText = `Your service request for ${service_request.name} was succesfully submitted`
+          
+          console.log('returnText: ' , returnText)
+          //console.log('res.sessionToken: ' , res.sessionToken)
+          return {
+            text: returnText,
+            options: { shouldEndSession: true },
+            session: res.session
+          }
+      } else if (value === 'no' || value === 'not' || value === 'nope') {
+        
+        return {
+          text: item.rejectionMsg,
+          options: { shouldEndSession: true }
+        }
+      }
+    }
+    
+    return {
+        directives: [
+          {
+            type: 'Dialog.ElicitSlot',
+            slotToElicit: 'confirmationSlot'
+          }
+        ],
+        text: item.confirmationMsg,
+        reprompt: item.repromptMsg,
+        options: { shouldEndSession: false },
+        session: {}  
+    }
 }
 
 export default doRequestIroning
