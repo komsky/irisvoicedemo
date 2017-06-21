5130be558ad98d4975687b886c046bbaf19209aa
Golan Shay <golanshy@gmail.com>
2017-06-21 18:13:39 +0100

    changes

--- src/server/handlers/in_room_dining.js
+++ src/server/handlers/in_room_dining.js
@@ -14,13 +14,12 @@ const getFoodInformation = async (payload) => {
   const { intent: { slots: { mainOptions: { name , value, confirmationStatus } } }, dialogState } = payload.request
 
   const res = await get(pathName, payload)
-  
+  const items = res.responses[0][getCategoryItems.key].content.categoryItems
+  const itemsText = items.map(x => x.name).join(',')
+  const returnText = `Please select from the following; ${itemsText}`
+
   // dialogState: 'STARTED' IN_PROGRESS
   if (dialogState == 'STARTED') {
-
-      const items = res.responses[0][getCategoryItems.key].content.categoryItems
-      const itemsText = items.map(x => x.name).join(',')
-      const returnText = `Please select from the following; ${itemsText}`
 
       return {
             directives: [
@@ -38,10 +37,51 @@ const getFoodInformation = async (payload) => {
   if (value !== null && value !== ``) {
 
       console.log(`value = ` , value)
-      const returnText = `You have selected ${value}; let me get order that for you`
+      if (value === `cancel` ) {
+        return {
+          text: `Your order was cancelled, thank you for using our voice command services`,
+          options: { shouldEndSession: true },
+          session: res.session
+        }
+      }
 
+      if (value === `steak` || value === `flatiron steak`) {
+
+      } else if (value === `burger` || value === `h burger` || value === `hamburger`) {
+        
+      } else if (value === `Lobster` || value === `Lobster Bisque`) {
+        
+      } else if (value === `Cheese burger`) {
+        
+      } else if (value === `Chocolate cake` || value === `cake`) {
+        
+      } else if (value === `Seafood paella` || value === `paella`) {
+        
+      } else if (value === `Caesar salad` || value === `salad`) {
+        
+      } else {
+        return {
+            directives: [
+                  {
+                    type: 'Dialog.ElicitSlot',
+                    slotToElicit: 'mainOptions'
+                  }
+                ],
+                text: returnText,
+                reprompt: `I didn\'t quite catch that. Could you repeat please?`,
+                options: { shouldEndSession: false },
+                session: {}  
+          }
+      }
+      
       return {
-          text: returnText,
+          directives: [
+                  {
+                    type: 'Dialog.ElicitSlot',
+                    slotToElicit: 'mainOptions'
+                  }
+                ],
+          text: `You have selected ${value}; is that correct?`,
           options: { shouldEndSession: true },
           session: res.session
       }
