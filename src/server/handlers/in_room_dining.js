81a7eb1e49f0f286b82e03434fa618374fe1ffbb
Golan Shay <golanshy@gmail.com>
2017-06-21 17:38:25 +0100

    changes

--- src/server/handlers/in_room_dining.js
+++ src/server/handlers/in_room_dining.js
@@ -11,36 +11,43 @@ const pathName = formatPath(items.code, getCategoryItems.path)
 
 const getFoodInformation = async (payload) => {
 
-  const { intent: { slots: { mainOptions: { name, value } } }, dialogState } = payload.request
+  const { intent: { slots: { mainOptions: { name, confirmationStatus } } }, dialogState } = payload.request
 
   const res = await get(pathName, payload)
   const items = res.responses[0][getCategoryItems.key].content.categoryItems
   const itemsText = items.map(x => x.name).join(',')
   const returnText = `Please select from the following; ${itemsText}`
 
-  if (name == null || name == '') {
+  // dialogState: 'STARTED' IN_PROGRESS
+  if (IN_PROGRESS == 'STARTED') {
       return {
-          text: 'You have selected ${value}',
+            
+            directives: [
+                  {
+                    type: 'Dialog.ElicitSlot',
+                    slotToElicit: 'mainOptions'
+                  }
+                ],
+                text: returnText,
+                reprompt: 'I didn\'t quite catch that. Could you repeat please?',
+                options: { shouldEndSession: false },
+                session: {}  
+          }
+  }
+  if (name !== null && name !== '') {
+
+      console.log('name = ' , name)
+      return {
+          text: 'You have selected ${name}',
           options: { shouldEndSession: true },
           session: res.session
       }
   } else {
-      console.log('name = ' , name)
+    
       return {
-      //text: returnText,
-      //options: { shouldEndSession: true },
-      //session: res.session
-
-      directives: [
-            {
-              type: 'Dialog.ElicitSlot',
-              slotToElicit: 'mainOptions'
-            }
-          ],
-          text: returnText,
-          reprompt: 'I didn\'t quite catch that. Could you repeat please?',
-          options: { shouldEndSession: false },
-          session: {}  
+          text: 'Sorry, didn\'t get that; please try again',
+          options: { shouldEndSession: true },
+          session: res.session
       }
   }
 }
